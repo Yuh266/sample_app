026cba98e2e1dce37d84b91fabfd6d5adae62fbd
Yuh266 <nhuy04625@gmail.com>
2025-04-16 15:55:01 +0700

    Chuonwg 3

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,13 +1,43 @@
 require "test_helper"
 
+
+
+
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+
+  test "should get root" do
+    get root_url  # Thay FILL_IN bằng helper root_url để gửi yêu cầu GET đến trang root
+    assert_response :success  # Kiểm tra xem phản hồi có thành công hay không (HTTP 200)
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "Help | #{@base_title}"
   end
+
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+    assert_select "title", "About | #{@base_title}"
+  end
+
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
+    
+  end
+
+
 end
